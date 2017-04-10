4e489aed53af6eff7829547a9e0e5d8f01563cf4
andreevr <romanize@hotmail.com>
2017-04-10 23:22:51 +0200

    RA: Some consistency asserts

--- java/World.java
+++ java/World.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class World {
 	
-	private int busCapacity;
-	private int numberOfStations;
+	private int busCapacity;      // C
+	private int numberOfStations; // N
 	private int busPosition;
 	private List<Integer> busPassengers;
 	private List<List<Integer>> stationsQueues;
@@ -49,23 +49,28 @@ public class World {
 	}
 	
 	public void move(Response response) {
+		List<Integer> M = response.getM();
+		int s = response.getS();
 		
-		for (int i : response.getM()) {
+		// http://stackoverflow.com/questions/3018683/what-does-the-assert-keyword-do
+		// Assertions have to be enabled with "java -ea Main"
+		this.checkConsistency(this.busCapacity, this.numberOfStations, this.busPosition, this.busPassengers, this.stationsQueues, M, s);
+		
+		for (int i : M) {
 			this.busPassengers.add(this.stationsQueues.get(this.busPosition).get(i));
 		}
 		
-		List<Integer> M = response.getM();
 		Collections.sort(M, new Comparator<Integer>() {
 		    public int compare(Integer n1, Integer n2) {
 		        return n2.compareTo(n1);
 		    }
 		});
-		List<Integer> S = this.stationsQueues.get(this.busPosition);
-		for (int i : M) {
-			S.remove(i);
-		}
+		//
+		List<Integer> q = this.stationsQueues.get(this.busPosition);
+		//
+		for (int i : M) q.remove(i);
 		
-		this.busPosition = (this.busPosition + (this.numberOfStations + response.getS())) % this.numberOfStations;
+		this.busPosition = (this.busPosition + (this.numberOfStations + s)) % this.numberOfStations;
 		
 		if (this.busPassengers.size() > 0) {
 			this.busPassengers.removeAll(Collections.singleton(this.busPosition));
@@ -94,32 +99,82 @@ public class World {
 		return busPosition;
 	}
 
-	public void setBusPosition(int busPosition) {
-		this.busPosition = busPosition;
-	}
-
 	public List<Integer> getBusPassengers() {
 		return busPassengers;
-	}
-
-	public void setBusPassengers(List<Integer> busPassengers) {
-		this.busPassengers = busPassengers;
 	}
 
 	public List<List<Integer>> getStationsQueues() {
 		return stationsQueues;
 	}
 
-	public void setStationsQueues(List<List<Integer>> stationsQueues) {
-		this.stationsQueues = stationsQueues;
-	}
-
-	public int getI() {
+	public int geti() {
 		return i;
 	}
 
-	public void setI(int i) {
+	public void seti(int i) {
 		this.i = i;
 	}
-	
+
+	public void checkConsistency(int C, int N, int b, List<Integer> B, List<List<Integer>> Q, List<Integer> M, int s) {
+		// 0.
+        // C is an integer >= 1
+        // N is an integer >= 2
+
+        assert(C >= 1);
+        assert(N >= 2);
+
+        // 1.
+        // b is an integer 0 <= b < N denoting
+        //   the current location of the bus.
+
+        assert((0 <= b) && (b < N));
+
+        // 2.
+        // B is a list [n1, n2, ..] of
+        //   the destinations of the passengers
+        //   currently on the bus
+        //   (not exceeding the capacity), i.e.
+        //   nk is the destination of passenger k.
+        //   The order is that of boarding
+        //   (provided by this function: see M).
+        //   No destination is the current position.
+
+        assert(B.stream().allMatch(n -> ((0 <= n) && (n < N))));
+        assert(B.stream().allMatch(n -> (n != b)));
+        
+        // 3.
+        // Q is a list of N lists, where
+        //   Q[n] = [t1, t2, ..] is the list of
+        //   people currently waiting at station n
+        //   with destinations t1, t2, ..
+        //   No destination equals the location,
+        //   i.e. (t != n) for any t in Q[n].
+
+        assert(Q.size() == N);
+        assert(Q.stream().allMatch(q -> q.stream().allMatch(n -> ((0 <= n) && (n < N)))));
+        for (int n = 0; n != Q.size(); n++)
+			for (int t : Q.get(n))
+				assert(t != n);
+
+        // 4.
+        // M is a list of indices M = [i1, i2, .., im]
+        //   into the list Q[b] indicating that
+        //   the people Q[b][i] will board the bus
+        //   (in the order defined by M).
+        //   Set M = [] if no one boards the bus.
+        //   Note the constraints:
+        //     len(B) + len(M) <= Capacity C,
+        //   and
+        //     0 <= i < len(Q[b]) for each i in M.
+
+        assert(M.stream().allMatch(i -> ((0 <= i) && (i < Q.get(b).size()))));
+        assert(B.size() + M.size() <= C);
+
+        // 5.
+        // s is either +1, -1, or 0, indicating
+        //   the direction of travel of the bus
+        //   (the next station is (b + s) % N).
+
+        assert((s == -1) || (s == 0) || (s == 1));
+	}
 }
